a33e983f9426df0b8f37183bda3f33abf5ef061a
Asif Parvez Sarker <asif.sarker@selisegroup.com>
2025-12-16 12:45:36 +0000

    Shadow Dom added

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+const htmlTemplate = document.createElement('template');
+htmlTemplate.innerHTML = ` <style> h1{background-color: yellow}</style>
+<h1> I am asif </h1>
+`
+
+class UserCard extends HTMLElement{
+    constructor(){
+        super();
+        const shadow = this.attachShadow({mode: 'open'})
+        shadow.append(htmlTemplate.content.cloneNode(true));
+    }
+}
+
+customElements.define('user-card', UserCard);
